e443b95bfacfab563ba14600f0d334d6eaafa0f4
Techffodils Technologies LLP <32633457+techffodils@users.noreply.github.com>
2023-08-01 09:57:36 +0530

    change

--- application/config/mimes.php
+++ application/config/mimes.php
@@ -179,5 +179,7 @@ return array(
 	'odt'	=>	'application/vnd.oasis.opendocument.text',
 	'odm'	=>	'application/vnd.oasis.opendocument.text-master',
 	'ott'	=>	'application/vnd.oasis.opendocument.text-template',
-	'oth'	=>	'application/vnd.oasis.opendocument.text-web'
+	'oth'	=>	'application/vnd.oasis.opendocument.text-web',
+	'avif' => array('image/avif', 'image/avif-sequence'),
+    'webp' => array('image/webp', 'image/webp-gallery', 'image/webp-ll', 'image/webp-sequence'),
 );
